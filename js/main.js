2f4225ba62975cfdf11f4da3223a5c079c01c0c9
NightTwoMoons <54476147+NightTwoMoons@users.noreply.github.com>
2020-06-01 03:46:52 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import Customixator from './customizator.js';
+import Customizator from './customizator.js';
+
+window.addEventListener('DOMContentLoaded', () => {
+    const panel = new Customizator();
+    panel.render();
+});
